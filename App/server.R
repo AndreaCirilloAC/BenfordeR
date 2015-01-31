6256cb16cf6c51345c686d82fbbb53ba1121eb21
andreacirilloac <andreacirilloac@gmail.com>
2015-01-31 07:00:01 +0100

    added logo file to show on the app

--- App/server.R
+++ App/server.R
@@ -1,23 +1,31 @@
 
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
+library(benford.analysis)
+#define left function
+left = function (string,char){
+  substr(string,1,char)}
+data(corporate.payment)
 
 shinyServer(function(input, output) {
-
-  output$distPlot <- renderPlot({
-
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+  
+  data                   <- reactive({if (is.null(input$records$name)){
+    corporate.payment$Amount }else{
+      read.csv(input$records$datapath,header=TRUE,sep=";")
+    }
   })
-
+  benford_obj            <- reactive({benford(data(),input$digits)})
+  output$benford_plot    <- renderPlot({
+    
+    plot(benford_obj())
+  })
+  output$benford_table   <- renderDataTable({suspectsTable(benford_obj())})
+  output$benford_suspect <- renderDataTable({
+    digits                   <-left(data(),input$digits)
+    data_output              <- data.frame(data(),digits)
+    suspects                 <- suspectsTable(benford_obj())
+    suspects_digits          <- (as.character(suspects$digits))
+    data_suspected           <- subset(data_output,as.character(data_output[,2])
+                                       %in% suspects_digits)
+  }) 
+  
 })
--- App/ui.R
+++ App/ui.R
@@ -8,23 +8,38 @@
 library(shiny)
 
 shinyUI(fluidPage(
-
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-
+  
+  tags$head(
+    tags$style(HTML("
+                    @import url('//fonts.googleapis.com/css?family=Lobster|Cabin:400,700');
+                    "))
+    ),
+  # Application logo and title
+  br(),
+  fluidRow(
+    column(1,img(src = "logo.png", height = 70, width = 70)),
+    column(11,h1("BenfordeR", style = "font-family: 'Helvetica Neue', cursive;
+                 font-weight: 500; line-height: 1.1; 
+                 color: #55ACEE;"))),
+  br(),
+  
   # Sidebar with a slider input for number of bins
   sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
+    sidebarPanel(width=3,
+                 h3("instructions"),
+                 p ("here come the instructions"),
+                 fileInput("records", h4("load file"),
+                           multiple = FALSE),
+                 numericInput("digits",h4("number of digits to test"),1)
     ),
-
+    
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("distPlot")
+      plotOutput("benford_plot",width= "100%"),
+      dataTableOutput("benford_table"),
+      dataTableOutput("benford_suspect")
+      
+      
     )
   )
-))
+    ))
